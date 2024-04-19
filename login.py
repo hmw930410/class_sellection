bdb73b2625075ee0ba69f0a5e05f2a57c6110131
syuan05 <syuan930510@gmail.com>
2024-04-20 03:46:15 +0800

    0420

--- login.py
+++ login.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import MySQLdb
 
 login = Flask(__name__)
 
+
+login.secret_key = 'key'
 # MySQL 連線設置
 db = MySQLdb.connect(host="127.0.0.1",
                      user="test",
@@ -32,17 +34,27 @@ def login_action():
     user = cursor.fetchone()
 
     if user:
+        session['username'] = username
         return render_template('course_schedule.html')
     else:
         return render_template('index.html', error="帳號或密碼錯誤")
 
 
-@login.route('/success', methods=['POST'])
-def success():
-    go_login = request.form.get("inside")
-    if go_login:
-        return redirect('/course_schedule.html')
+@login.route('/course_schedule', methods=['GET'])
+def course_schedule():
+    username = session.get('username')
+    if username:
+        cursor.execute("""
+            SELECT coures.course_ID, course.course_name, course.class_time, course.teacher, course.classroom
+            FROM course 
+            INNER JOIN classtable ON course.course_ID = classtable.course_ID
+            WHERE classtable.student_ID = %s
+        """, (username,))
+        courses = cursor.fetchall()  # 獲取查詢結果
 
+        return render_template('course_schedule.html', courses=courses)  # 關閉資料庫連接
+    else:
+        return redirect('/')
 
 if __name__ == '__main__':
     login.run(debug=True)

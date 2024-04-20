f1a965fcfb97350d6c41300df13800f6646ce3a3
syuan05 <syuan930510@gmail.com>
2024-04-21 06:44:23 +0800

    0421

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import login
 import show_student_info
 import show_student_table
 import show_canchoose_table
@@ -9,22 +10,42 @@ app.secret_key = "key"
 
 
 @app.route('/')
-def page():
+def index():
     return render_template('index.html')
 
 
-@app.route('/schedule', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def loginAction():
+    if request.method == 'POST':  # 檢查用戶的請求是否是 POST 方法
+        username = request.form['username']
+        password = request.form['password']
+        session_data = login.Action(username, password)
+        if session_data is not None:
+            session['username'] = username
+            return redirect('/schedule')
+        else:
+            error = "Incorrect username/password"
+            return render_template('index.html', error=error)
+    else:
+        return render_template('index.html')
+
+
+@app.route('/schedule')
 def studentInfo():
-    nowstudent = 2
-    students = show_student_info.student_info(nowstudent)
-    add_need_course.addcourse(nowstudent)
-    hastables = show_student_table.student_table(nowstudent)
-    cantables = show_canchoose_table.canchoose(nowstudent)
-    return render_template(
-        'course_schedule.html',
-        students=students,
-        hastables=hastables,
-        cantables=cantables)
+    student_id = session.get('username')
+    if student_id is not None:
+        print(student_id)
+        students = show_student_info.student_info(student_id)
+        add_need_course.addcourse(student_id)
+        hastables = show_student_table.student_table(student_id)
+        cantables = show_canchoose_table.canchoose(student_id)
+        return render_template(
+            'course_schedule.html',
+            students=students,
+            hastables=hastables,
+            cantables=cantables)
+    else:
+        return redirect(url_for('loginAction'))
 
 
 if __name__ == '__main__':
--- login.py
+++ login.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import session
 import MySQLdb
 
-def Action(username):
+
+def Action(username, password):
 
     try:
         db = MySQLdb.connect(host="127.0.0.1",
@@ -10,23 +11,17 @@ def Action(username):
                              db="show_sql")
         cursor = db.cursor()
 
-        cursor.execute("SELECT class_ID FROM student WHERE student_ID = %s",
-                       (username,))
-        class_ID = cursor.fetchone()[0]
-        cursor.execute("""
-            SELECT course.course_ID, course.course_name, course.need,
-                   course.class_time, course.teacher, course.classroom
-            FROM course
-            WHERE course.need = "必修"
-            AND open_class = (SELECT classname FROM class_list WHERE class_ID =
-            %s) """, (class_ID,))
-        table = cursor.fetchall()
-        insert_haschoose.haschoose(table, username)
-        renew_student_credits.haschoose(username)
-        students = show_student_info.student_info(username)
-        return render_template(
-            'course_schedule.html',
-            students=students)
+        cursor.execute("""SELECT *
+                          FROM student
+                          WHERE student_ID = %s AND PASSWORD = %s
+                          """, (username, password,))
+        account = cursor.fetchone()
+        print(account)
+        if account:
+            session['success'] = True
+            session['username'] = account['id']
+            return True
+
     except Exception as e:
         print("Error:", e)
-        return None
+        return False
